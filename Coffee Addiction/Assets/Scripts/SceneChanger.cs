86f78cf1b7457ae770802ea111db91bf8d659c2e
Cockamamie <bugrymenko@yandex.ru>
2021-05-26 12:55:53 +0500

    trying

---
+++ Coffee Addiction/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+	public void Change(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
