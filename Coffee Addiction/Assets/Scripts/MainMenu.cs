5e74043a73e1ccabd61fd9ef83857266a859fc4d
AnastasiiaProkopeva <Anastasiiaprokopeva31@gmail.com>
2021-05-26 07:31:23 +0500

    Набросок меню

---
+++ Coffee Addiction/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Rules(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
